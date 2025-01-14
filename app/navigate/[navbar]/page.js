f01bb77d6808cd55ee6381caa9c931cf428620d1
AYOU-pixel <ayoubpc938@gmail.com>
2025-01-14 18:15:37 +0100

    improve header

--- app/navigate/[navbar]/page.js
+++ app/navigate/[navbar]/page.js
@@ -1,8 +1,17 @@
-
-export default function Navigate(props) {
+// pages/navigate/[navbar].js
+export async function generateStaticParams() {
+    // Define the possible values for the `navbar` parameter
+    return [
+      { navbar: 'home' },
+      { navbar: 'about' },
+      { navbar: 'contact' },
+    ];
+  }
+  
+  export default function Navigate(props) {
     return (
-        <div>
-            <h1>nav</h1>
-        </div>
+      <div>
+        <h1>nav</h1>
+      </div>
     );
-}
+  }
